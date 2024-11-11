839b0e8a71012f2b86658fe6f92ded95f5ea0ad4
rkappes <rkappes18@gmail.com>
2024-11-10 21:13:48 -0800

    objects exercise

--- 07-javascript/03-objects.js
+++ 07-javascript/03-objects.js
@@ -2,33 +2,39 @@ let result = {};
 
 const insertItem = (key, value) => {
   // insertItem the property with key and value into result
+  result[key] = "value";
   return key;
 };
 
 const deleteItem = (key) => {
   // remove the property with key from result
+  delete result.key;
   return key;
 };
 
 const lookupItem = (key) => {
   // return the value from result that is associated with key
+  console.log(key);
   return key;
 };
 
 const printItems = () => {
   // return a string of the concatenated key in result, separated by commas
+  for (let key in result) {
+    console.log(key);
+  }
 };
 
-insertItem('hello', 'world');
-insertItem('lorem', 'ipsum');
-insertItem('sit', 'amet');
+insertItem("hello", "world");
+insertItem("lorem", "ipsum");
+insertItem("sit", "amet");
 console.log(printItems());
 // expected: 'hello, lorem, sit' (order may be different)
-console.log(lookupItem('lorem'));
+console.log(lookupItem("lorem"));
 // expected: 'ipsum'
 
-deleteItem('lorem');
+deleteItem("lorem");
 console.log(printItems());
 // expected: 'hello, sit' (order may be different)
-console.log(lookupItem('lorem'));
+console.log(lookupItem("lorem"));
 // expected: 'Item does not exist'
