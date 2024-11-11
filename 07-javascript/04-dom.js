1172e5e4ece1e6f8d96a910d35f59eca2cd17769
rkappes <rkappes18@gmail.com>
2024-11-10 21:13:57 -0800

    fix typos

--- 07-javascript/04-dom.js
+++ 07-javascript/04-dom.js
@@ -2,9 +2,9 @@
 // when the user clicks the 'copy' button, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
-let userInput = document.querySelector("#userInpute1");
+let userInput = document.querySelector("#userInput1");
 let copy = document.querySelector("#copy");
-let output = document.querySelector("#output");
+let output = document.querySelector(".output");
 
 // add an event listener on the target element
 copy.addEventListener("click", handleClick);
@@ -18,7 +18,7 @@ function handleClick(event) {
 // when the user enters input text, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
-let userInput2 = document.querySelector("#userInpute2");
+let userInput2 = document.querySelector("#userInput2");
 
 // add an event listener on the target element
 userInput2.addEventListener("input", handleInput);
