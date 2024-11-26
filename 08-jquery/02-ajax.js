e65b4a8ac30e7bffa1fcfbe024628e4f8e3d7630
rkappes <rkappes18@gmail.com>
2024-11-25 20:34:05 -0800

    add styling and error message

--- 08-jquery/02-ajax.js
+++ 08-jquery/02-ajax.js
@@ -6,6 +6,12 @@ $(document).ready(() => {
 
     $("#books").append(
       $("<div>")
+        .css({
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "20px",
+        })
         .append($("<h3>").text(item.name))
         .append($("<p>").text(item.authors[0]))
         .append($("<p>").text(item.released.substr(0, 4)))
@@ -27,6 +33,13 @@ $(document).ready(() => {
 
       error: (error) => {
         console.error(error);
+        $("#books").append(
+          $("<div>").text(`An error occured. Please try again`)
+        );
+      },
+
+      complete: () => {
+        $("#loading").remove();
       },
     });
   };
